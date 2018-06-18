d68f6a90705feee8e38960b68ba6a5134c30f211
Francisco Javier Lendínez Tirado <fjlendinez@outlook.es>
2018-06-18 23:22:45 +0200

    Update antispam.py

--- antispambot/antispam.py
+++ antispambot/antispam.py
@@ -12,10 +12,11 @@ class AntiSpamBot:
             name = message.new_chat_participant.first_name
             if hasattr(message.new_chat_participant, 'last_name') and message.new_chat_participant.last_name is not None:
                 name += " {}".format(message.new_chat_participant.last_name)
-
-            if spammers in name:
-                bot.delete_message(message.chat.id, message.id)
-                bot.kick_chat_member(message.chat.id, message.new_chat_participant.id)
+            
+            for spammer in spammers:
+                if spammer in name:
+                    bot.delete_message(message.chat.id, message.id)
+                    bot.kick_chat_member(message.chat.id, message.new_chat_participant.id)
         self.bot = bot
 
     def run(self):
